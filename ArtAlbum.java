c9ee491224b8f9a83a0d6939f2c5f0cfeccee4ac
HLiliana <hetea.liliana@yahoo.com>
2023-03-19 11:08:19 +0200

    Resubmit homeworkLibraryCatalog after feedback.

---
+++ ArtAlbum.java
@@ -0,0 +1,13 @@
+package libraryCatalog;
+
+/** here we made a subclass for class Books
+ * it contains a variable type String for page quality
+ */
+    class ArtAlbum extends Book {
+    public String paperQuality;
+
+    public ArtAlbum(String name, int numberOfPages, String paperQuality){
+        super(name, numberOfPages);
+        this.paperQuality=paperQuality;
+    }
+}
---
+++ Novel.java
@@ -0,0 +1,15 @@
+package libraryCatalog;
+
+/**here we made a subclass for class Books
+ * it contains a variable String type for Novels(like mystery, sf, etc.)
+ */
+
+class Novel extends Book {
+    public String typeOfNovel;
+
+    public Novel(String name, int numberOfPages, String typeOfNovel) {
+        super(name, numberOfPages);
+        this.typeOfNovel = typeOfNovel;
+    }
+}
+
